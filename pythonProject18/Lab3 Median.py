01df62dcefe682dff93e2c5b9c62239094b1e0c9
Xienina Roldan <89092718+xieninaroldan@users.noreply.github.com>
2024-02-14 09:47:55 +0800

    Add files via upload

---
+++ pythonProject18/Lab3 Median.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('banana.jpg')
+img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+blur = cv.medianBlur(img,25)
+blur_rgb = cv.cvtColor(blur, cv.COLOR_BGR2RGB)
+
+plt.subplot(121),plt.imshow(img_rgb),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(122),plt.imshow(blur_rgb),plt.title('Blurred')
+plt.xticks([]),plt.yticks([])
+plt.show()
